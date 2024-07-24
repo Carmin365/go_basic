b5e5ca272e1342d220c3d4c35cfa53855aea68f0
Carmin Ilarraz <144161912+Carmin365@users.noreply.github.com>
2024-07-24 09:19:39 -0300

    Update exThreeBasicGo.go

---
+++ exThreeBasicGo.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    age := 30
+
+    if age >= 18 {
+        fmt.Println("You are of legal age!")
+    } else {
+        fmt.Println("You are underage!")
+    }
+}
